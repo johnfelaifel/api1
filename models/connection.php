50e0db8851c55903f825c87d4672a9910df09c4b
John Dairo Felaifel Lopez <johnfelaifel@gmail.com>
2023-12-26 21:14:14 -0500

    update3023-12-26(2)

--- models/connection.php
+++ models/connection.php
@@ -1,8 +1,13 @@
 <?php
+require_once "vendor/autoload.php";
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
 
-class Connection{
+class Connection
+{
 
-    static public function infoDatabase(){        
+    static public function infoDatabase()
+    {
         $infoDB = array(
             "database" => "courses",
             "user" => "john",
@@ -14,27 +19,55 @@ class Connection{
     }
 
 
-    static public function connect(){
+    static public function connect()
+    {
         try {
             $link = new PDO(
-                "mysql:host=localhost;dbname=".Connection::infoDatabase()["database"],
+                "mysql:host=localhost;dbname=" . Connection::infoDatabase()["database"],
                 Connection::infoDatabase()["user"],
                 Connection::infoDatabase()["password"]
             );
             //$link = new PDO('mysql:host=localhost;dbname=prueba', $usuario, $contraseña);
             $link->exec("set names utf8");
         } catch (PDOException $e) {
-            die("Error: ".$e->getMessage());
+            die("Error: " . $e->getMessage());
         }
         return $link;
     }
 
-    static public function getColumnsData($table){
+    static public function getColumnsData($table)
+    {
         $databese = Connection::infoDatabase()['database'];
         $sql = "SELECT COLUMN_NAME AS item FROM information_schema.columns WHERE table_schema = '$databese' AND table_name = '$table'";
         return Connection::connect()
-        ->query($sql)
-        ->fetchAll(PDO::FETCH_OBJ);
+            ->query($sql)
+            ->fetchAll(PDO::FETCH_OBJ);
+    }
+
+    static public function jwt($id, $email)
+    {
+        $time = time();
+        $token = array(
+            "iat" => $time,
+            "exp" => $time + (60*60*24), //Expiracion en 1 dia
+            "data" => [
+                "id" => $id,
+                "email" => $email
+            ]
+        );
+        $jwt = JWT::encode($token, "01234567899876543210", 'HS256');
+        print_r($jwt);
+    }
+
+    static public function validarAppykey(){
+        if(!isset(getallheaders()["Autorization"])  || (getallheaders()["Autorization"] != "1234567890") ){
+            $json = array(
+                'status' => 400,
+                'result' => "You are not authorizes to make this request"
+            );
+            echo json_encode($json,http_response_code($json["status"]));
+            return;
+        }
     }
 
 }
--- routes/routes.php
+++ routes/routes.php
@@ -1,6 +1,14 @@
 <?php
 $routesArray = array_filter(explode("/api1/",$_SERVER['REQUEST_URI']));
 $table = array_filter(explode("?",$routesArray[1]));
+
+//crear validar jwtoken
+//$jwt = Connection::jwt("22","john@gmail.com");
+
+//Validar key en la cabecera de la peticion, la cabecera debe llamarse "Autorization" para este ejemplo con la clave 1234567890
+//Connection::validarAppykey();
+//return;
+
 if(count($routesArray) == 0){
     $json = array(
         'status' => 404,
